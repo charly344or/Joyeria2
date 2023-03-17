be6695bd38c4a206a967bc662735bfbbee0fe1c8
charly344or <c.andresrc@hotmail.com>
2023-03-17 14:38:43 -0500

    Se agrega el pryecto

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2023 <a href="Universitaria Uniagustiniana" target="_blank">Jean Paul Serrato & Carlos Andres</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
